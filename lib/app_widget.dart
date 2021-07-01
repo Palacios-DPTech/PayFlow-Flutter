227b1bc00c189a0be26dab339942180454002e3f
Damian Palacios <damian_nunes@hotmail.com>
2021-07-01 04:04:55 -0300

    Add files via upload

---
+++ lib/app_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'modules/barcode_scanner/barcode_scanner_page.dart';
+import 'modules/insert_boleto/insert_boleto_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(
+              user: ModalRoute.of(context)!.settings.arguments as UserModel,
+            ),
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(
+              barcode: ModalRoute.of(context) != null
+                  ? ModalRoute.of(context)!.settings.arguments.toString()
+                  : null,
+            ),
+      },
+    );
+  }
+}
---
+++ lib/modules/meus_boletos/meus_boletos_page.dart
@@ -0,0 +1,71 @@
+import 'package:animated_card/animated_card.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_style.dart';
+import 'package:payflow/shared/widgets/boleto_info/boleto_info_widget.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
+
+class MeusBoletosPage extends StatefulWidget {
+  MeusBoletosPage({Key? key}) : super(key: key);
+
+  @override
+  _MeusBoletosPageState createState() => _MeusBoletosPageState();
+}
+
+class _MeusBoletosPageState extends State<MeusBoletosPage> {
+  final controller = BoletoListController();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            child: Stack(
+              children: [
+                Container(
+                  height: 40,
+                  color: AppColors.primary,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: ValueListenableBuilder<List<BoletoModel>>(
+                    valueListenable: controller.boletosNotifier,
+                    builder: (_, boletos, __) => AnimatedCard(
+                      direction: AnimatedCardDirection.top,
+                      child: BoletoInfoWidget(
+                        size: boletos.length,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+            child: Row(
+              children: [
+                Text("Meus boletos", style: TextStyles.titleBoldHeading),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Divider(
+              color: AppColors.stroke,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: BoletoListWidget(
+              controller: controller,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
